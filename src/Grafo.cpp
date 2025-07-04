ffceb895b813a6f5eaa2dc3d80b5043c2005f230
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-07-04 18:48:14 -0300

    Comeca adicionar transitivo Direto

--- src/Grafo.cpp
+++ src/Grafo.cpp
@@ -1,5 +1,6 @@
 #include "Grafo.h"
-
+#include <set>
+#include <stack>
 
 Grafo::Grafo() {
 }
@@ -7,9 +8,56 @@ Grafo::Grafo() {
 Grafo::~Grafo() {
 }
 
+
 vector<char> Grafo::fecho_transitivo_direto(char id_no) {
-    cout<<"Metodo nao implementado"<<endl;
-    return {};
+    set<char> visitados;
+    stack<No*> pilha;
+    vector<char> resultado;
+
+    // Encontrar o nó inicial
+    No* no_inicial = nullptr;
+    for (No* no : lista_adj) {
+        if (no->id == id_no) {
+            no_inicial = no;
+            break;
+        }
+    }
+
+    if (!no_inicial) {
+        cout << "Nó não encontrado: " << id_no << endl;
+        return {};
+    }
+
+    pilha.push(no_inicial);
+    visitados.insert(id_no);
+
+    while (!pilha.empty()) {
+        No* atual = pilha.top();
+        pilha.pop();
+
+        for (Aresta* aresta : atual->arestas) {
+            char id_vizinho = aresta->id_no_alvo;
+
+            if (visitados.find(id_vizinho) == visitados.end()) {
+                // Procurar o nó correspondente na lista de adjacência
+                No* vizinho = nullptr;
+                for (No* no : lista_adj) {
+                    if (no->id == id_vizinho) {
+                        vizinho = no;
+                        break;
+                    }
+                }
+
+                if (vizinho) {
+                    visitados.insert(id_vizinho);
+                    pilha.push(vizinho);
+                    resultado.push_back(id_vizinho);
+                }
+            }
+        }
+    }
+
+    return resultado;
 }
 
 vector<char> Grafo::fecho_transitivo_indireto(char id_no) {
