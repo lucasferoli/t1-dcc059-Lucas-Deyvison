c53ff5f9d9c459c4820ba5bd455d55ecf3ace4bd
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-07-07 23:02:12 -0300

    Adiciona Caminho mínimo Floyd-Warshall

--- src/Gerenciador.cpp
+++ src/Gerenciador.cpp
@@ -4,20 +4,21 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
 
 void Gerenciador::comandos(Grafo* grafo) {
-    cout<<"Digite uma das opcoes abaixo e pressione enter:"<<endl<<endl;
-    cout<<"(a) Fecho transitivo direto de um no;"<<endl;
-    cout<<"(b) Fecho transitivo indireto de um no;"<<endl;
-    cout<<"(c) Caminho minimo (Djikstra);"<<endl;
-    cout<<"(d) Caminho minimo (Floyd);"<<endl;
-    cout<<"(e) Arvore Geradora Minima (Algoritmo de Prim);"<<endl;
-    cout<<"(f) Arvore Geradora Minima (Algoritmo de Kruskal);"<<endl;
-    cout<<"(g) Arvore de caminhamento em profundidade;"<<endl;
-    cout<<"(h) Raio, diametro, centro e periferia do grafo;"<<endl;
-    cout<<"(0) Sair;"<<endl<<endl;
+    cout << "Digite uma das opcoes abaixo e pressione enter:" << endl << endl;
+    cout << "(a) Fecho transitivo direto de um no;" << endl;
+    cout << "(b) Fecho transitivo indireto de um no;" << endl;
+    cout << "(c) Caminho minimo (Djikstra);" << endl;
+    cout << "(d) Caminho minimo (Floyd);" << endl;
+    cout << "(e) Arvore Geradora Minima (Algoritmo de Prim);" << endl;
+    cout << "(f) Arvore Geradora Minima (Algoritmo de Kruskal);" << endl;
+    cout << "(g) Arvore de caminhamento em profundidade;" << endl;
+    cout << "(h) Raio, diametro, centro e periferia do grafo;" << endl;
+    cout << "(0) Sair;" << endl << endl;
 
     char resp;
     cin >> resp;
@@ -74,92 +75,97 @@ void Gerenciador::comandos(Grafo* grafo) {
         }
 
         case 'c': {
-
             char id_no_1 = get_id_entrada();
             char id_no_2 = get_id_entrada();
-            vector<char> caminho_minimo_dijkstra = grafo->caminho_minimo_dijkstra(id_no_1,id_no_2);
-            cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+            vector<char> caminho_minimo_dijkstra = grafo->caminho_minimo_dijkstra(id_no_1, id_no_2);
+            cout << "Metodo de impressao em tela nao implementado" << endl << endl;
 
-            if(pergunta_imprimir_arquivo("caminho_minimo_dijkstra.txt")) {
-                cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+            if (pergunta_imprimir_arquivo("caminho_minimo_dijkstra.txt")) {
+                cout << "Metodo de impressao em arquivo nao implementado" << endl;
             }
-
-
             break;
         }
 
         case 'd': {
+            // Caminho mínimo usando o algoritmo de Floyd-Warshall
+            cout << "\nCaminho minimo por Floyd-Warshall" << endl;
+            char de = get_id_entrada();   // Solicita o vértice de origem
+            char para = get_id_entrada(); // Solicita o vértice de destino
 
-            char id_no_1 = get_id_entrada();
-            char id_no_2 = get_id_entrada();
-            vector<char> caminho_minimo_floyd = grafo->caminho_minimo_floyd(id_no_1,id_no_2);
-            cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+            vector<char> rota = grafo->caminho_minimo_floyd(de, para); // Calcula o caminho mínimo
 
-            if(pergunta_imprimir_arquivo("caminho_minimo_floyd.txt")) {
-                cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+            if (!rota.empty()) {
+                // Exibe a rota encontrada na tela
+                cout << "\nRota encontrada (" << rota.size() << " vertices):\n";
+                for (size_t passo = 0; passo < rota.size(); passo++) {
+                    cout << rota[passo];
+                    if (passo != rota.size() - 1) cout << " -> ";
+                }
+                cout << endl;
             }
 
-
+            // Pergunta se deseja salvar o resultado em arquivo
+            if (pergunta_imprimir_arquivo("floyd_path.txt")) {
+                ofstream saida("floyd_path.txt");
+                if (saida) {
+                    // Salva a rota no arquivo
+                    saida << "De: " << de << "\nPara: " << para << "\nRota: ";
+                    copy(rota.begin(), rota.end(), ostream_iterator<char>(saida, " "));
+                    cout << "Arquivo salvo com sucesso!" << endl;
+                }
+            }
             break;
         }
+
         case 'e': {
-
             int tam;
-            cout<<"Digite o tamanho do subconjunto: ";
-            cin>>tam;
+            cout << "Digite o tamanho do subconjunto: ";
+            cin >> tam;
 
-            if(tam > 0 && tam <= grafo->ordem) {
-
-                vector<char> ids = get_conjunto_ids(grafo,tam);
+            if (tam > 0 && tam <= grafo->ordem) {
+                vector<char> ids = get_conjunto_ids(grafo, tam);
                 Grafo* arvore_geradora_minima_prim = grafo->arvore_geradora_minima_prim(ids);
-                cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+                cout << "Metodo de impressao em tela nao implementado" << endl << endl;
 
-                if(pergunta_imprimir_arquivo("agm_prim.txt")) {
-                    cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                if (pergunta_imprimir_arquivo("agm_prim.txt")) {
+                    cout << "Metodo de impressao em arquivo nao implementado" << endl;
                 }
 
                 delete arvore_geradora_minima_prim;
-
-            }else {
-                cout<<"Valor invalido"<<endl;
+            } else {
+                cout << "Valor invalido" << endl;
             }
-
             break;
         }
 
         case 'f': {
-
             int tam;
-            cout<<"Digite o tamanho do subconjunto: ";
-            cin>>tam;
+            cout << "Digite o tamanho do subconjunto: ";
+            cin >> tam;
 
-            if(tam > 0 && tam <= grafo->ordem) {
-
-                vector<char> ids = get_conjunto_ids(grafo,tam);
+            if (tam > 0 && tam <= grafo->ordem) {
+                vector<char> ids = get_conjunto_ids(grafo, tam);
                 Grafo* arvore_geradora_minima_kruskal = grafo->arvore_geradora_minima_kruskal(ids);
-                cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+                cout << "Metodo de impressao em tela nao implementado" << endl << endl;
 
-                if(pergunta_imprimir_arquivo("agm_kruskal.txt")) {
-                    cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                if (pergunta_imprimir_arquivo("agm_kruskal.txt")) {
+                    cout << "Metodo de impressao em arquivo nao implementado" << endl;
                 }
 
                 delete arvore_geradora_minima_kruskal;
-
-            }else {
-                cout<<"Valor invalido"<<endl;
+            } else {
+                cout << "Valor invalido" << endl;
             }
-
             break;
         }
 
         case 'g': {
-
             char id_no = get_id_entrada();
             Grafo* arvore_caminhamento_profundidade = grafo->arvore_caminhamento_profundidade(id_no);
-            cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+            cout << "Metodo de impressao em tela nao implementado" << endl << endl;
 
-            if(pergunta_imprimir_arquivo("arvore_caminhamento_profundidade.txt")) {
-                cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+            if (pergunta_imprimir_arquivo("arvore_caminhamento_profundidade.txt")) {
+                cout << "Metodo de impressao em arquivo nao implementado" << endl;
             }
 
             delete arvore_caminhamento_profundidade;
@@ -167,77 +173,72 @@ void Gerenciador::comandos(Grafo* grafo) {
         }
 
         case 'h': {
-
             int raio = grafo->raio();
             int diametro = grafo->diametro();
             vector<char> centro = grafo->centro();
             vector<char> periferia = grafo->periferia();
 
-            cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+            cout << "Metodo de impressao em tela nao implementado" << endl << endl;
 
-            if(pergunta_imprimir_arquivo("raio_diametro_centro_periferia.txt")) {
-                cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+            if (pergunta_imprimir_arquivo("raio_diametro_centro_periferia.txt")) {
+                cout << "Metodo de impressao em arquivo nao implementado" << endl;
             }
-
             break;
         }
+
         case '0': {
             exit(0);
         }
+
         default: {
-            cout<<"Opção inválida"<<endl;
+            cout << "Opção inválida" << endl;
         }
     }
 
     comandos(grafo);
-
 }
 
 char Gerenciador::get_id_entrada() {
-    cout<<"Digite o id de um no: ";
+    cout << "Digite o id de um no: ";
     char id;
-    cin>>id;
-    cout<<endl;
+    cin >> id;
+    cout << endl;
     return id;
 }
 
-vector<char> Gerenciador::get_conjunto_ids(Grafo *grafo, int tam) {
+vector<char> Gerenciador::get_conjunto_ids(Grafo* grafo, int tam) {
     vector<char> ids = {};
-    while((int)ids.size() < tam) {
-        char id_no =get_id_entrada();
+    while ((int)ids.size() < tam) {
+        char id_no = get_id_entrada();
         bool existe = false;
-        for(No* no: grafo->lista_adj){
-            if(no->id == id_no){
+        for (No* no : grafo->lista_adj) {
+            if (no->id == id_no) {
                 existe = true;
                 break;
             }
         }
 
-        if(!existe){
-            cout<<"Vertice nao existe"<<endl;
-        }else{
-            bool repetido = find(ids.begin(), ids.end(),id_no) != ids.end();
-            if(repetido) {
-                    cout<<"Valor repetido"<<endl;
-            }else {
+        if (!existe) {
+            cout << "Vertice nao existe" << endl;
+        } else {
+            bool repetido = find(ids.begin(), ids.end(), id_no) != ids.end();
+            if (repetido) {
+                cout << "Valor repetido" << endl;
+            } else {
                 ids.push_back(id_no);
             }
         }
-
     }
-
     return ids;
 }
 
-
 bool Gerenciador::pergunta_imprimir_arquivo(string nome_arquivo) {
-
-    cout<<"Imprimir em arquivo externo? ("<<nome_arquivo<<")"<<endl;
-    cout<<"(1) Sim;"<<endl;
-    cout<<"(2) Nao."<<endl;
+    cout << "Imprimir em arquivo externo? (" << nome_arquivo << ")" << endl;
+    cout << "(1) Sim;" << endl;
+    cout << "(2) Nao." << endl;
     int resp;
-    cin>>resp;
-    cout<<endl;
+    cin >> resp;
+    cout << endl;
 
     switch (resp) {
         case 1:
@@ -245,7 +246,7 @@ bool Gerenciador::pergunta_imprimir_arquivo(string nome_arquivo) {
         case 2:
             return false;
         default:
-            cout<<"Resposta invalida"<<endl;
+            cout << "Resposta invalida" << endl;
             return pergunta_imprimir_arquivo(nome_arquivo);
     }
 }
--- src/Grafo.cpp
+++ src/Grafo.cpp
@@ -4,6 +4,10 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <unordered_map>
+#include <limits>
+
+
 
 Grafo::Grafo() {
 }
@@ -194,10 +198,79 @@ vector<char> Grafo::caminho_minimo_dijkstra(char id_no_a, char id_no_b) {
     return {};
 }
 
-vector<char> Grafo::caminho_minimo_floyd(char id_no, char id_no_b) {
-    cout<<"Metodo nao implementado"<<endl;
-    return {};
+vector<char> Grafo::caminho_minimo_floyd(char id_no_a, char id_no_b) {
+    // Define um valor grande para representar "infinito" (ausência de caminho)
+    const int MAX_INT = 999999999;
+    int total_vertices = lista_adj.size();
+
+    // Mapeia cada id de vértice (char) para um índice inteiro e vice-versa
+    unordered_map<char, int> char_para_int;
+    unordered_map<int, char> int_para_char;
+    
+    // Preenche os mapas de conversão entre char <-> int para os vértices
+    for (int indice = 0; indice < total_vertices; indice++) {
+        char_para_int[lista_adj[indice]->id] = indice;
+        int_para_char[indice] = lista_adj[indice]->id;
+    }
+
+    // Verifica se os vértices de origem e destino existem no grafo
+    if (!char_para_int.count(id_no_a) || !char_para_int.count(id_no_b)) {
+        cout << "Erro: Vertice(s) invalido(s)!" << endl;
+        return vector<char>();
+    }
+
+    // Cria as matrizes de distâncias e de próximos vértices para reconstrução do caminho
+    vector<vector<int>> distancias(total_vertices, vector<int>(total_vertices, MAX_INT));
+    vector<vector<int>> proximo_no(total_vertices, vector<int>(total_vertices, -1));
+
+    // Inicializa as matrizes:
+    // - distância para si mesmo é 0
+    // - distância para vizinhos diretos é o peso da aresta
+    // - registra o próximo nó para reconstrução do caminho
+    for (int i = 0; i < total_vertices; i++) {
+        distancias[i][i] = 0;
+        for (auto& conexao : lista_adj[i]->arestas) {
+            int j = char_para_int[conexao->id_no_alvo];
+            distancias[i][j] = conexao->peso;
+            proximo_no[i][j] = j;
+        }
+    }
+
+    // Algoritmo de Floyd-Warshall:
+    // Para cada possível vértice intermediário, verifica se passar por ele reduz a distância entre pares de vértices
+    for (int intermediario = 0; intermediario < total_vertices; intermediario++) {
+        for (int partida = 0; partida < total_vertices; partida++) {
+            for (int chegada = 0; chegada < total_vertices; chegada++) {
+                int novo_caminho = distancias[partida][intermediario] + distancias[intermediario][chegada];
+                if (distancias[partida][intermediario] != MAX_INT &&
+                    distancias[intermediario][chegada] != MAX_INT &&
+                    novo_caminho < distancias[partida][chegada]) {
+                    distancias[partida][chegada] = novo_caminho;
+                    proximo_no[partida][chegada] = proximo_no[partida][intermediario];
+                }
+            }
+        }
+    }
+
+    // Reconstrói o caminho mínimo do vértice de origem até o destino usando a matriz de próximos vértices
+    int inicio = char_para_int[id_no_a];
+    int fim = char_para_int[id_no_b];
+    if (proximo_no[inicio][fim] == -1) {
+        cout << "Caminho inexistente entre " << id_no_a << " e " << id_no_b << endl;
+        return vector<char>();
+    }
+
+    vector<char> trajeto;
+    int atual = inicio;
+    while (atual != fim) {
+        trajeto.push_back(int_para_char[atual]);
+        atual = proximo_no[atual][fim];
+    }
+    trajeto.push_back(int_para_char[fim]);
+
+    return trajeto;
 }
+
 
 Grafo * Grafo::arvore_geradora_minima_prim(vector<char> ids_nos) {
     cout<<"Metodo nao implementado"<<endl;
